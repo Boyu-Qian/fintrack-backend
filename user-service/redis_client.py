7d8285a3e1abaff5da16f1c954c50310e4bb1188
Boyu Qian <101378701+Boyu-Qian@users.noreply.github.com>
2025-10-26 21:47:07 -0400

    update

--- user-service/redis_client.py
+++ user-service/redis_client.py
@@ -6,11 +6,4 @@ HOST = os.getenv("REDIS_HOST")
 PORT = int(os.getenv("REDIS_PORT"))
 PASSWORD = os.getenv("REDIS_PASSWORD")
 DB = 0
-time.sleep(5)
 redis_client = redis.Redis(host=HOST,port=PORT,db=DB,password=PASSWORD,decode_responses=True)
-
-try:
-    if redis_client.ping():
-        print("Redis up!")
-except redis.RedisError as e:
-    print(f"Redis Error: {e}")
