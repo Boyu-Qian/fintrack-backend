c87f7f190d5dba52298d0d58343e6cd4494bc3d7
Boyu Qian <101378701+Boyu-Qian@users.noreply.github.com>
2025-10-26 21:42:58 -0400

    update

--- user-service/redis_client.py
+++ user-service/redis_client.py
@@ -1,11 +1,12 @@
 import os
 import redis
+import time
 
 HOST = os.getenv("REDIS_HOST")
 PORT = int(os.getenv("REDIS_PORT"))
 PASSWORD = os.getenv("REDIS_PASSWORD")
 DB = 0
-
+time.sleep(5)
 redis_client = redis.Redis(host=HOST,port=PORT,db=DB,password=PASSWORD,decode_responses=True)
 
 try:
